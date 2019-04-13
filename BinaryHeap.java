ad83b8c64349730f3681c843019e19354122f1b5
djbarajas <35737585+djbarajas@users.noreply.github.com>
2019-04-13 09:35:16 -0700

    commented Binary Heap

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -3,12 +3,21 @@ import java.util.Arrays;
 public class BinaryHeap {
     private int size;
     private int[] data;
-
+    /*
+    * BinaryHeap()
+    * creates an array for the Heap of size 10 as specified in the
+    * assignment and the amount of elements to 0
+     */
     public BinaryHeap(){
         data = new int[10];
         size = 0;
     }
-
+    /*
+    *add()
+    * if the new size is equal to the array length then grow the array
+    * else, add the item to the end and swap the new child with the
+    * parent when necessary.
+     */
     public void add(int item){
         if(size+1 == data.length){
             grow();
@@ -22,14 +31,22 @@ public class BinaryHeap {
             parent = (parent-1)/2;
         }
     }
-
+    /*
+    * swap()
+    * swaps 2 elements in an array
+     */
     private void swap(int[] data, int i, int j) {
         int temp = data[i];
         data[i] = data[j];
         data[j] = temp;
     }
-
-    public int remove()  {
+    /*
+    * remove()
+    * first, make the first element the last position and subtract from the size
+    * if the size is grater than 0 then do the shift down method which checks if
+    * the children are smaller than the parent and swaps when necessary
+     */
+    public int remove() {
         //Assert.not_false(size != 0);
         swap(data,size-1,0);
         --size;
@@ -39,6 +56,13 @@ public class BinaryHeap {
         return data[size];
     }
 
+    /*
+    * shiftDown()
+    * while the node is not a leaf node, compare the 2 children
+    * to find the smallest and if the parent is smaller then return
+    * but if the child is smaller then swap the child with the parent
+    * and make the new position the child to go down a level
+    */
     private void shiftDown(int pos) {
         int child;
         while(!(isLeaf(pos))){
@@ -54,11 +78,19 @@ public class BinaryHeap {
         }
     }
 
+    /*
+    * isLeaf()
+    * if position is within those 2 numbers then it must be a leaf
+    * node and therefore the shiftdown method can end
+     */
     private boolean isLeaf(int pos){
         return ((pos >= (size)/2) && pos < size);
     }
 
-
+    /*
+    * grow()
+    * doubles the size of the BinaryHeap
+     */
     private void grow(){
         data = Arrays.copyOf(data,data.length*2);
     }
